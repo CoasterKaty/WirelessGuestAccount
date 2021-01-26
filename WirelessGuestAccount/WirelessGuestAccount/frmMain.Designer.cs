4f772b9fc1ec3f0eddd70a230c6c026ca23aa17d
CoasterKaty <62079949+CoasterKaty@users.noreply.github.com>
2021-01-26 21:37:43 +0000

    Add files via upload

---
+++ WirelessGuestAccount/WirelessGuestAccount/frmMain.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WirelessGuestAccount
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdReset = new System.Windows.Forms.Button();
+            this.cboValidity = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numPassCount = new System.Windows.Forms.NumericUpDown();
+            this.cmdPrint = new System.Windows.Forms.Button();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPassCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmdReset);
+            this.groupBox1.Controls.Add(this.cboValidity);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numPassCount);
+            this.groupBox1.Controls.Add(this.cmdPrint);
+            this.groupBox1.Controls.Add(this.txtDetails);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 245);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(475, 229);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Print Passes";
+            // 
+            // cmdReset
+            // 
+            this.cmdReset.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdReset.Location = new System.Drawing.Point(257, 183);
+            this.cmdReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdReset.Name = "cmdReset";
+            this.cmdReset.Size = new System.Drawing.Size(117, 28);
+            this.cmdReset.TabIndex = 5;
+            this.cmdReset.Text = "Clear";
+            this.cmdReset.UseVisualStyleBackColor = true;
+            this.cmdReset.Click += new System.EventHandler(this.cmdReset_Click);
+            // 
+            // cboValidity
+            // 
+            this.cboValidity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboValidity.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboValidity.FormattingEnabled = true;
+            this.cboValidity.Items.AddRange(new object[] {
+            "1 day",
+            "2 days",
+            "5 days",
+            "30 days",
+            "Never Expires"});
+            this.cboValidity.Location = new System.Drawing.Point(156, 118);
+            this.cboValidity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboValidity.Name = "cboValidity";
+            this.cboValidity.Size = new System.Drawing.Size(160, 26);
+            this.cboValidity.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Validity Period";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of Passes";
+            // 
+            // numPassCount
+            // 
+            this.numPassCount.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPassCount.Location = new System.Drawing.Point(156, 78);
+            this.numPassCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numPassCount.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numPassCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPassCount.Name = "numPassCount";
+            this.numPassCount.Size = new System.Drawing.Size(73, 26);
+            this.numPassCount.TabIndex = 2;
+            this.numPassCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cmdPrint
+            // 
+            this.cmdPrint.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdPrint.Location = new System.Drawing.Point(100, 183);
+            this.cmdPrint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdPrint.Name = "cmdPrint";
+            this.cmdPrint.Size = new System.Drawing.Size(117, 28);
+            this.cmdPrint.TabIndex = 4;
+            this.cmdPrint.Text = "Print Passes";
+            this.cmdPrint.UseVisualStyleBackColor = true;
+            this.cmdPrint.Click += new System.EventHandler(this.cmdPrint_Click);
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDetails.Location = new System.Drawing.Point(156, 32);
+            this.txtDetails.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.Size = new System.Drawing.Size(304, 26);
+            this.txtDetails.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guest Details";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(8, 11);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(491, 230);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = resources.GetString("lblInfo.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 481);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Guest Wireless Pass Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPassCount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboValidity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numPassCount;
+        private System.Windows.Forms.Button cmdPrint;
+        private System.Windows.Forms.TextBox txtDetails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdReset;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ WirelessGuestAccount/WirelessGuestAccount/frmMain.cs
@@ -0,0 +1,191 @@
+﻿/* frmMain
+ * Main form for Wireless Guest Manager
+ * Katy Nicholson
+ * https://katynicholson.uk
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.IO.Ports;
+
+namespace WirelessGuestAccount
+{
+    public partial class frmMain : Form
+    {
+        Settings kSettings;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboValidity.SelectedIndex = 1;
+            kSettings = new Settings(true, "config.xml", AppDomain.CurrentDomain.BaseDirectory);
+            lblInfo.Text = kSettings.Get("LabelText", "Guests must have a wireless pass in order to access the guest wireless. Passes are valid for 1, 2, 5 or 30 days, or unlimited.\n\"Guest Details\" is not printed on the ticket. This is optional for short length passes but required for 30 day or unlimited passes.\nInstructions for connecting will be on the printed ticket, they will need to use the $SSID$ network and the supplied username/password.\nStaff and students should use the $SSID$ network with their existing school login details and should not be using guest logins.");
+            lblInfo.Text = lblInfo.Text.Replace("$SSID$", kSettings.Get("SSID", "SSID"));
+        }
+
+        private bool checkUserExists(PrincipalContext namingContext, string strUsername)
+        {
+            using (var user = UserPrincipal.FindByIdentity(namingContext, strUsername))
+            {
+                if (user != null)
+                {
+                    user.Dispose();
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void printTicket(SerialPort port, string strUsername, string strPassword, DateTime dtValidity, bool bolExpires)
+        {
+            DateTime dtExpires = dtValidity.AddDays(-1);
+            
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            port.Write(ascii.GetString(new byte[] { 27, 97, 1 }));
+            // Print the logo in ID 1
+            port.Write(ascii.GetString(new byte[] { 28, 112, 1, 0 }));
+            port.WriteLine("");
+            port.Write(ascii.GetString(new byte[] { 27, 82, 3 }));
+            port.Write(ascii.GetString(new byte[] { 27, 33, 185 }));
+            port.WriteLine("Guest Wireless Details");
+            port.WriteLine("");
+           
+            port.Write(ascii.GetString(new byte[] { 27, 33, 0 }));
+         // aaaaaaaaaaaaaaa123456789012345678901234567890123456789012
+            port.WriteLine("Connect your device to the " + kSettings.Get("SSID", "SSID"));
+            port.WriteLine("network using the following details:");
+            port.WriteLine("");
+            port.WriteLine("Username: " + strUsername);
+            port.WriteLine("Password: " + strPassword);
+            port.WriteLine("");
+            port.WriteLine("Apple Devices: Press Trust when prompted");
+            port.WriteLine("for certificate.");
+            port.WriteLine("");
+            port.WriteLine("Other Devices: Select these if prompted:");
+            port.WriteLine("EAP Method: PEAP");
+            port.WriteLine("CA Certificate: Don't Validate");
+            port.WriteLine("Identity: " + strUsername);
+            port.WriteLine("Anonymous Identity: (leave blank)");
+            port.WriteLine("");
+            if (bolExpires)
+            {
+                port.WriteLine("This pass is valid until the end of:");
+                port.WriteLine(dtExpires.ToShortDateString());
+            }
+            else
+            {
+                port.WriteLine("This pass does not expire.");
+            }
+
+
+            port.WriteLine("");
+            port.Write(ascii.GetString(new byte[] { 29, 86, 65, 0 }));
+        }
+        private void cmdPrint_Click(object sender, EventArgs e)
+        {
+            cmdPrint.Enabled = false;
+            cmdPrint.Text = "Printing...";
+            try
+            {
+                using (var namingContext = new PrincipalContext(ContextType.Domain, null, kSettings.Get("DN", "ou=Guest Wireless,DC=contoso,DC=com")))
+                {
+
+                    using (SerialPort port = new SerialPort(kSettings.Get("Port", "COM7"), 19200, Parity.None, 8, StopBits.One))
+                    {
+                        port.Open();
+                        DateTime dtExpires = DateTime.Now;
+                        bool bolExpires = true;
+                        switch (cboValidity.SelectedIndex)
+                        {
+                            case 0:
+                                //1 day
+                                dtExpires = DateTime.Now.AddDays(2);
+                                break;
+                            case 1:
+                                // 2 days
+                                dtExpires = DateTime.Now.AddDays(3);
+                                break;
+                            case 2:
+                                // 5 days
+                                dtExpires = DateTime.Now.AddDays(6);
+                                break;
+                            case 3:
+                                //30 days
+                                dtExpires = DateTime.Now.AddDays(31);
+                                break;
+                            case 4:
+                                //unlimited
+                                bolExpires = false;
+                                break;
+
+                        }
+                        using (GroupPrincipal grpWifi = GroupPrincipal.FindByIdentity(namingContext, kSettings.Get("Group", "G GuestWireless")))
+                        {
+                            for (int intCount = 1; intCount <= numPassCount.Value; intCount++)
+                            {
+                                // Generate accounts
+                                bool bolUserExists = true;
+                                string strUsername = "";
+                                string strPassword = "";
+                                // Username is guest-xxxx where xxxx are random digits, obtained by generating a GUID and picking 4 digits.
+                                while (bolUserExists)
+                                {
+                                    string strAccountGuid = Guid.NewGuid().ToString();
+                                    strUsername = "guest-" + strAccountGuid.Substring(0, 4);
+                                    strPassword = strAccountGuid.Substring(24, 6);
+                                    bolUserExists = checkUserExists(namingContext, strUsername);
+                                }
+
+                                //Create user
+
+                                using (var newUser = new UserPrincipal(namingContext))
+                                {
+                                    newUser.SamAccountName = strUsername;
+                                    newUser.UserPrincipalName = strUsername + kSettings.Get("UPNSuffix", "@contoso.com");
+                                    newUser.Name = strUsername;
+                                    if (txtDetails.Text.Length > 0) newUser.Description = txtDetails.Text;
+                                    newUser.Enabled = true;
+                                    newUser.SetPassword(strPassword);
+                                    if (bolExpires) newUser.AccountExpirationDate = dtExpires;
+                                    newUser.PermittedWorkstations.Add(kSettings.Get("Server", "CONTOSOSERVER1"));
+                                    newUser.Save();
+                                    grpWifi.Members.Add(namingContext, IdentityType.UserPrincipalName, strUsername + kSettings.Get("UPNSuffix", "@contoso.com"));
+                                }
+                                // Print tickets
+                                printTicket(port, strUsername, strPassword, dtExpires, bolExpires);
+                            }
+                            grpWifi.Save();
+                        }
+                        port.Close();
+                    }
+                }
+            }
+            catch (Exception dsException)
+            {
+                MessageBox.Show(dsException.Message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            cmdPrint.Text = "Print Passes";
+            cmdPrint.Enabled = true;
+        }
+
+
+        private void cmdReset_Click(object sender, EventArgs e)
+        {
+            txtDetails.Text = "";
+            cboValidity.SelectedIndex = 1;
+            numPassCount.Value = 1;
+            txtDetails.Focus();
+
+        }
+    }
+}
